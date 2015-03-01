2c94a5e64730ed3671058a0c62122c130464ce23
Wil <schockwil@gmail.com>
2015-02-28 22:14:40 -0700

    First commit

---
+++ desktop/src/com/twentyfortyeight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.twentyfortyeight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.twentyfortyeight.TwentyFortyEight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TwentyFortyEight(), config);
+	}
+}
